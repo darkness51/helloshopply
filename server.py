5dfbc811e4b2c3d6b66a8830731baa67d73c4c02
Carlos Aguilar <caguilar@dwdandsolutions.com>
2012-07-01 20:06:21 -0600

    Added function to start tornado app

--- server.py
+++ server.py
@@ -1,8 +1,14 @@
 from tornado.web import Application
 from hello_shopply import HelloShopplyServiceHandler
 
+import tornado.ioloop
+
 API = Application([
             (r'/helloshopply', HelloShopplyServiceHandler),
 
         ],
     )
+    
+if __name__ == "__main__":
+    API.listen(8888)
+    tornado.ioloop.IOLoop.instance().start()
