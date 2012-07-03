168b7e4fbc708d60545f560df494b5d27184b193
Carlos Aguilar <caguilar@dwdandsolutions.com>
2012-07-03 16:42:24 -0600

    Added package details

--- setup.py
+++ setup.py
@@ -4,5 +4,10 @@ from setuptools import setup, find_packages
 import sys
 
 setup(
-    
+    name = "Hello Shopply",
+    version = 0.1,
+    description = "Test Application",
+    packages = find_packages(),
+    author = "Carlos Aguilar",
+    author_email = "caguilar@dwdandsolutions.com"
 )
